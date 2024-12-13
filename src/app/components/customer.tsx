82d6c8a76fd434ca021c706ffa72cb427a2ee694
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-12-14 00:48:59 +0700

    feat: add our products part 2

--- src/app/components/customer.tsx
+++ src/app/components/customer.tsx
@@ -18,29 +18,25 @@ export default function Customer() {
           {
             description:
               "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur!",
-            image:
-              "https://plus.unsplash.com/premium_photo-1717529137991-510ad3be15d9?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "image/products.png",
             name: "Đông Y ÔNG BỤT",
           },
           {
             description:
               "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur!",
-            image:
-              "https://images.unsplash.com/photo-1566753323558-f4e0952af115?q=80&w=1921&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "image/products.png",
             name: "Đông Y ÔNG BỤT",
           },
           {
             description:
               "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur!",
-            image:
-              "https://images.unsplash.com/photo-1560298803-1d998f6b5249?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "image/products.png",
             name: "Đông Y ÔNG BỤT",
           },
           {
             description:
               "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa nostrum labore dolor facilis, nesciunt facere mollitia nam aspernatur!",
-            image:
-              "https://images.unsplash.com/photo-1518287010730-4386819bf3e9?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "image/products.png",
             name: "Đông Y ÔNG BỤT",
           },
         ]}
