32b9254d654b1d599b36732e475b7f2ba9fb780c
idontwannapickaname <lightrageg@gmail.com>
2025-02-07 21:58:13 +0700

    change a tag to next-link

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 
@@ -5,8 +6,8 @@ export default function Footer( ){
     return (
       <footer className="flex flex-row justify-between px-10 py-5 bg-black">
         <div className="flex flex-row gap-6 mobile:flex-col mobile:gap-2 ">
-          <a className="font-bold text-lg" href="https://github.com" target="_blank">GITHUB</a>
-          <a className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</a>
+          <Link className="font-bold text-lg" href="https://github.com" target="_blank">GITHUB</Link>
+          <Link className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</Link>
         </div>
         <div className="mobile:flex tablet:flex mobile:place-self-end tablet:place-self-end">
           <p className="font-bold text-lg">© 2024 CYSLABS</p>
