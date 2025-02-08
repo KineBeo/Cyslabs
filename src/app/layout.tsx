d7fb82b8ae1f2239e8a96d99395ab507ed89d039
idontwannapickaname <lightrageg@gmail.com>
2025-02-08 20:33:42 +0700

    seo implements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,59 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Cyslabs",
+  metadataBase: new URL('https://cyslabs.vercel.app/'),
+  title: {
+    default: 'Cyslabs',
+    template: '%s | Cyslabs'
+  },
   description: "The coolest cybersecurity lab in Vietnam",
+  keywords: ["cyslabs", "cybersecurity", "vietnam"],
+  icons: [
+    {
+      url: "/favicon.ico",
+      rel: "icon",
+      type: "image/x-icon",
+    },
+    {
+      url: "/favicon.ico",
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+    },
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    type: "website",
+    locale: "vi_VN",
+    url: "https://cyslabs.vercel.app/",
+    title: "Cyslabs",
+    description: "The coolest cybersecurity lab in Vietnam",
+    siteName: "Cyslabs",
+    images: [
+      {
+        url: "/favicon.ico"
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cyslabs',
+    description: 'The coolest cybersecurity lab in Vietnam',
+    images: ['/favicon.ico'],
+  },
+  manifest: '/manifest.ts',
+  verification: {
+    google: 'your_google_verification_code',
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="vi" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
