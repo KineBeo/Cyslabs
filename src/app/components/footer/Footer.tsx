1d72b1229b7a0c143c43309b20f09714e61dedbd
TruongHoang2004 <22028035@vnu.edu.vn>
2025-02-19 16:06:17 +0700

    feat: add url

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
-export default function Footer( ){
-    return (
-      <footer className="flex flex-row justify-between px-10 py-5 bg-black">
-        <div className="flex flex-row gap-6 mobile:flex-col mobile:gap-2 ">
-          <Link className="font-bold text-lg" href="https://github.com" target="_blank">GITHUB</Link>
-          <Link className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</Link>
-        </div>
-        <div className="mobile:flex tablet:flex mobile:place-self-end tablet:place-self-end">
-          <p className="font-bold text-lg">© 2024 CYSLABS</p>
-        </div>
-      </footer>
-    );
+export default function Footer() {
+  return (
+    <footer className="flex flex-row justify-between bg-black px-10 py-5">
+      <div className="flex flex-row mobile:flex-col gap-6 mobile:gap-2">
+        <Link className="font-bold text-lg" href="https://github.com/KineBeo" target="_blank">GITHUB</Link>
+        <Link className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</Link>
+      </div>
+      <div className="tablet:flex mobile:flex mobile:place-self-end tablet:place-self-end">
+        <p className="font-bold text-lg">© 2025 CYSLABS</p>
+      </div>
+    </footer>
+  );
 }
