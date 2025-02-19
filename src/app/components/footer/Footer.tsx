efd50687801dc0933c57c1b2284bb81f43785d57
TruongHoang2004 <22028035@vnu.edu.vn>
2025-02-19 16:30:23 +0700

    feat: hidden footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="flex flex-row justify-between bg-black px-10 py-5">
       <div className="flex flex-row mobile:flex-col gap-6 mobile:gap-2">
         <Link className="font-bold text-lg" href="https://github.com/KineBeo" target="_blank">GITHUB</Link>
-        <Link className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</Link>
+        {/* <Link className="font-bold text-lg" href="https://cyslabs.vercel.app/" target="_blank">TELEGRAM</Link> */}
       </div>
       <div className="tablet:flex mobile:flex mobile:place-self-end tablet:place-self-end">
         <p className="font-bold text-lg">© 2025 CYSLABS</p>
